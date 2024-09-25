3baae8930470e119d3e4777a58c4a36d2d31ad40
SENG Seyha <sengseyha2001@gmail.com>
2024-09-25 14:51:08 +0700

    design and set up services for microservice

---
+++ back end/starter-parent/src/test/java/com/legend/starter_parent/StarterParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legend.starter_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
