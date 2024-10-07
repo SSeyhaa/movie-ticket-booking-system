072c355463c4acc949ce974923810b6aa508a730
SENG Seyha <sengseyha2001@gmail.com>
2024-10-07 16:56:37 +0700

    config spring boot multi-module such as starter-parent, common-util, and other biz-services

---
+++ back end/user-service/src/main/java/com/legend/user_service/entity/User.java
@@ -0,0 +1,31 @@
+//package com.legend.user_service.entity;
+//
+//import com.legend.common_util.audit.Auditable;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//@Entity
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Setter
+//@Getter
+//public class User extends Auditable {
+//
+//  @Id
+//  @GeneratedValue(strategy = GenerationType.IDENTITY)
+//  private Long id;
+//
+//  private String keycloakId;
+//  private String imagePath;
+//  private String firstName;
+//  private String lastName;
+//  private String email;
+//  private String phoneNumber;
+//  private String address;
+//}
