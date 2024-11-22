6bfe2b6dfacb08fe0568aae6dd4be6e051122f66
SENG Seyha <sengseyha2001@gmail.com>
2024-11-22 17:46:44 +0700

    feature: setup microservices infrastructure with Docker Compose

---
+++ back end/starter-parent/database/init.sql
@@ -0,0 +1,27 @@
+-- Create Keycloak database
+CREATE
+DATABASE keycloak_db;
+
+-- Create User database
+CREATE
+DATABASE user_db;
+
+-- Create Movie database
+CREATE
+DATABASE movie_db;
+
+-- Create Booking database
+CREATE
+DATABASE booking_db;
+
+-- Create Payment database
+CREATE
+DATABASE payment_db;
+
+-- Create Notification database
+CREATE
+DATABASE notification_db;
+
+-- Create Feedback database
+CREATE
+DATABASE feedback_db;
