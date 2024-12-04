bcd4dca8c04a4384f0800676b32781a31880ff3e
SENG Seyha <sengseyha2001@gmail.com>
2024-12-04 17:53:45 +0700

    sets the application JVM's default time zone to UTC

--- back end/user-service/src/main/java/com/legend/user_service/UserServiceApplication.java
+++ back end/user-service/src/main/java/com/legend/user_service/UserServiceApplication.java
@@ -1,5 +1,6 @@
 package com.legend.user_service;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
+		// Set default time zone to UTC
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
