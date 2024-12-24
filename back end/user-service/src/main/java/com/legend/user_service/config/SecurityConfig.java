90d93eb465ff0367f962c48e51d6d2dc2ecae6ce
SENG Seyha <sengseyha2001@gmail.com>
2024-12-24 11:41:17 +0700

    config centralized CORS and apply with security in user service

--- back end/user-service/src/main/java/com/legend/user_service/config/SecurityConfig.java
+++ back end/user-service/src/main/java/com/legend/user_service/config/SecurityConfig.java
@@ -1,19 +1,24 @@
 package com.legend.user_service.config;
 
 import com.legend.common_util.constant.PublicEndpoint;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+  private final CorsConfigurationSource corsConfigurationSource;
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.cors(Customizer.withDefaults())
+    http.cors(cors -> cors.configurationSource(corsConfigurationSource))
         .csrf(Customizer.withDefaults())
         .sessionManagement(
             session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
