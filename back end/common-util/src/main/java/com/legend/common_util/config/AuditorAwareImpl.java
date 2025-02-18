926dfb157e738259ad567cf4b1ea5e39d6233bcc
SENG Seyha <sengseyha2001@gmail.com>
2025-02-18 09:03:03 +0700

    [common-util]: refactor class AuditorAwareImpl

--- back end/common-util/src/main/java/com/legend/common_util/config/AuditorAwareImpl.java
+++ back end/common-util/src/main/java/com/legend/common_util/config/AuditorAwareImpl.java
@@ -3,17 +3,20 @@ package com.legend.common_util.config;
 import com.legend.common_util.util.CurrentUserUtils;
 import java.util.Optional;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.lang.NonNull;
 import org.springframework.security.core.Authentication;
+import org.springframework.util.StringUtils;
 
 public class AuditorAwareImpl implements AuditorAware<String> {
 
   @Override
+  @NonNull
   public Optional<String> getCurrentAuditor() {
 
     return CurrentUserUtils.getCurrentUserOptional()
         .filter(Authentication::isAuthenticated)
         .map(CustomJwtAuthenticationToken::getFullName)
-        .filter(name -> !name.isEmpty())
+        .filter(StringUtils::hasText)
         .or(() -> Optional.of("system"));
   }
 }
