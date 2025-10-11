7adea68c83c34d5708859d3e3226bedfb95a88a0
seyha.seng <sengseyha2001@gmail.com>
2025-10-11 19:03:36 +0700

    [common-util]: improve class ApiResponse

--- back end/common-util/src/main/java/kh/dev/common_util/dto/response/ApiResponse.java
+++ back end/common-util/src/main/java/kh/dev/common_util/dto/response/ApiResponse.java
@@ -1,18 +1,20 @@
 package kh.dev.common_util.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import java.time.ZonedDateTime;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@JsonPropertyOrder({"code", "status", "message", "timestamp", "data"})
 public class ApiResponse<T> {
 
   private final int code;
   private final String status;
   private final String message;
-  private final T data;
   private final ZonedDateTime timestamp;
+  private final T data;
 
   public ApiResponse(int code, String status, String message, T data) {
     this.code = code;
@@ -20,6 +22,10 @@ public class ApiResponse<T> {
     this.message = message;
     this.data = data;
     this.timestamp = ZonedDateTime.now();
+  }
+
+  public static <T> ApiResponse<T> success(String message, T data) {
+    return new ApiResponse<>(200, "OK", message, data);
   }
 
   public static <T> ApiResponse<T> success(T data) {
